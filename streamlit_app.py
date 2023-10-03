c1a1ba977bacfb6d6f12899dc8a424d716b0f510
geoffmatho <geoffmatheson@gmail.com>
2023-10-03 13:28:32 +1100

    I reckon this might do it. Filtering based on name.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,6 @@ team2 = st.selectbox("Team 2", CURRENT_TEAMS)
 st.write(team1, team2)
 
 players1 = [p.name for p in team1.players]
-st.write(players1)
-filteredPlayers = list(set(team1.players).intersection(team2.players))
+players2 = [p.name for p in team2.players]
+filteredPlayers = list(set(players1).intersection(players2))
 st.write(filteredPlayers)
