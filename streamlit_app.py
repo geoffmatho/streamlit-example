76564f5b41043a9e477d096bf1a84669a8b19a17
geoffmatho <geoffmatheson@gmail.com>
2023-10-03 23:54:40 +0000

    Added checkbox so it doesn't scrape for nothing

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,11 @@ team1 = st.selectbox("Team 1", CURRENT_TEAMS)
 team2 = st.selectbox("Team 2 (Includes options for weird teams)", ALL_TEAMS)
 
 st.write(team1, team2)
+runIt = st.checkbox("Ready to run?")
 
-players1 = [p.name for p in team1.players]
-players2 = [p.name for p in team2.players]
-filteredPlayers = list(set(players1).intersection(players2))
-st.write(filteredPlayers)
+if runIt:
+
+    players1 = [p.name for p in team1.players]
+    players2 = [p.name for p in team2.players]
+    filteredPlayers = list(set(players1).intersection(players2))
+    st.write(filteredPlayers)
